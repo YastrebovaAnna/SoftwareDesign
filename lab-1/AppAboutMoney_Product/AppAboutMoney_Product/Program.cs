6922a3c2acc90742fc476df5e19df8e87bfddafa
YastrebovaAnna <ipz221_yaav@student.ztu.edu.ua>
2024-05-03 23:37:44 +0300

    Changed program.cs

--- lab-1/AppAboutMoney_Product/AppAboutMoney_Product/Program.cs
+++ lab-1/AppAboutMoney_Product/AppAboutMoney_Product/Program.cs
@@ -5,26 +5,31 @@ class Program
     static void Main(string[] args)
     {
         UAH uah = new UAH(60, 20);
-        Product product = new Product("Apple", uah); 
+        Product product1 = new Product("Apple", uah); 
+        Product product2 = new Product("Banana", uah); 
         Warehouse warehouse = new Warehouse();
 
-        WarehouseProduct warehouseProduct = new WarehouseProduct(product, "kg", 150, DateTime.Now.AddDays(-10));
+        WarehouseProduct warehouseProduct1 = new WarehouseProduct(product1, "kg", 150, DateTime.Now.AddDays(-10));
+        WarehouseProduct warehouseProduct2 = new WarehouseProduct(product2, "kg", 180, DateTime.Now.AddDays(-20));
 
-        warehouse.AddProduct(warehouseProduct);
+        warehouse.AddProduct(warehouseProduct1);
+        warehouse.AddProduct(warehouseProduct2);
 
         Reporting reporting = new Reporting(warehouse);
 
         Console.WriteLine("Initial Inventory:");
         reporting.GenerateInventoryReport();
 
-        product.ReducePrice(0, 50); 
+        product1.ReducePrice(0, 50); 
+        product2.ReducePrice(1, 0);
         Console.WriteLine("\nInventory after price changes:");
         reporting.GenerateInventoryReport();
 
        
-        reporting.RegisterProductShipment(warehouseProduct);  
+        reporting.RegisterProductShipment(warehouseProduct2);  
 
-        Console.WriteLine("\nInventory after shipping some aplles:");
+        Console.WriteLine("\nInventory after shipping some bananas:");
+        reporting.GenerateInventoryReport();
 
         reporting.RegisterProductReception(new WarehouseProduct(new Product("Orange", new UAH(1, 75)), "kg", 100, DateTime.Now));
 
