3ef837bd70c97bc98c036727fbb1f1a7901bdc0d
YastrebovaAnna <ipz221_yaav@student.ztu.edu.ua>
2024-05-16 17:16:43 +0300

    Started 5 lab

---
+++ lab-5/ConsoleApp/LightHTML/LightElementNode.cs
@@ -0,0 +1,59 @@
+﻿using LightHTML.enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightHTML
+{
+    public class LightElementNode : LightNode
+    {
+        public string TagName { get; set; }
+        public DisplayType Display { get; set; }
+        public ClosingType Closing { get; set; }
+        public List<string> CssClasses { get; set; } = new List<string>();
+        public List<LightNode> Children { get; set; } = new List<LightNode>();
+
+        public LightElementNode(string tagName, DisplayType display, ClosingType closing)
+        {
+            TagName = tagName;
+            Display = display;
+            Closing = closing;
+        }
+        public void AddChild(LightNode child)
+        {
+            Children.Add(child);
+        }
+
+        public void AddCssClass(string cssClass)
+        {
+            CssClasses.Add(cssClass);
+        }
+        public override void OuterHTML()
+        {
+            Console.Write($"<{TagName}");
+
+            if (CssClasses.Count > 0)
+                Console.Write($" class=\"{string.Join(" ", CssClasses)}\"");
+
+            Console.Write(">");
+
+            if (Closing == ClosingType.Paired)
+            {
+                InnerHTML();
+                Console.Write($"</{TagName}>");
+            }
+            else
+                Console.Write("/>");
+        }
+
+        public void InnerHTML()
+        {
+            foreach (var child in Children)
+            {
+                child.OuterHTML();
+            }
+        }
+    }
+}
---
+++ lab-5/ConsoleApp/LightHTML/LightTextNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightHTML
+{
+    public class LightTextNode : LightNode
+    {
+        protected string Text { get; set; }
+
+        public LightTextNode(string text)
+        {
+            Text = text;
+        }
+        public override void OuterHTML()
+        {
+            Console.Write(Text);
+        }
+    }
+}
