27360fc8e0e666df38fcfc31ede1f607fee089ee
YastrebovaAnna <ipz221_yaav@student.ztu.edu.ua>
2024-05-17 00:58:13 +0300

    Command done

--- lab-5/ConsoleApp/ConsoleApp/Program.cs
+++ lab-5/ConsoleApp/ConsoleApp/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LightHTML;
+using LightHTML.command;
+using LightHTML.enums;
+using System.Windows.Input;
+using ICommand = LightHTML.command.ICommand;
+
+public class Program
+{
+    public static void Main()
+    {
+        LightElementNode elementNode = new LightElementNode("div", DisplayType.Block, ClosingType.Paired);
+
+        CommandManager commandManager = new CommandManager();
+
+        Console.WriteLine("Adding a CSS class 'new-class'");
+        ICommand addClassCommand = new AddCssClassCommand(elementNode, "new-class");
+        commandManager.ExecuteCommand(addClassCommand);
+        Console.Write("Current HTML: ");
+        elementNode.OuterHTML();
+        Console.WriteLine();
+
+        Console.WriteLine("Unadding a CSS class 'new-class'");
+        commandManager.Undo();
+        Console.Write("Current HTML: ");
+        elementNode.OuterHTML();
+        Console.WriteLine();
+
+        Console.WriteLine("Adding a CSS class 'another-class'");
+        elementNode.AddCssClass("another-class");
+        Console.Write("Current HTML: ");
+        elementNode.OuterHTML(); 
+        Console.WriteLine();
+
+        Console.WriteLine("Removing a CSS class 'another-class'");
+        ICommand removeClassCommand = new RemoveCssClassCommand(elementNode, "another-class");
+        commandManager.ExecuteCommand(removeClassCommand);
+        Console.Write("Current HTML: ");
+        elementNode.OuterHTML();
+        Console.WriteLine();
+
+        Console.WriteLine("Undoing the removal of a CSS class 'another-class'");
+        commandManager.Undo();
+        Console.Write("Current HTML: ");
+        elementNode.OuterHTML();
+        Console.WriteLine();
+    }
+}
---
+++ lab-5/ConsoleApp/LightHTML/command/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightHTML.command
+{
+    public interface ICommand
+    {
+        void Execute();
+        void Undo();
+    }
+}
